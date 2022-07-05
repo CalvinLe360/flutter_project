fbfcb2aae45454ff0ba5916448d2d35c4adc323b
CalvinLe360 <100341992+CalvinLe360@users.noreply.github.com>
2022-07-05 20:47:05 +0930

    startup name generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +11,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final appTitle = "Company Startup Name Generator";
     return MaterialApp(
-      title: "Welcome to Flutter",
+      title: appTitle,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to Flutter")
-          ),
-        body: const Center(
-          child: Text("Hello World"),
-          ),
+        appBar: AppBar(title: Text(appTitle)),
+        body: Center(child: RandomWords()),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(20),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return ListTile(
+            title: Text(
+              "${_suggestions[index].first} ${_suggestions[index].second}",
+              style: _biggerFont,
+            ),
+          );
+        });
   }
 }
